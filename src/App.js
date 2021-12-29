91d705567e32491bc55678f11ad68c32c00ea578
Ryan Clements <ryanclementshax@gmail.com>
2021-12-29 10:52:22 -0500

    create minimal reproduction case

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline text-used">Hello world!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        used: "rgb(var(--colors-used))",
+        unused: "rgb(var(--colors-unused))",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addBase }) => {
+      addBase({
+        ":root": {
+          "--colors-used": "0, 0, 255",
+          "--colors-unused": "255, 0, 0",
+        },
+      });
+    }),
+  ],
 };
